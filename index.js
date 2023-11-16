12176a303f8b009f67ffa89fb6b9aed902a1d55d
Iva <iva.ursic@fer.hr>
2023-11-16 11:18:55 +0100

    p12

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const path = require('path');
 const config = require('./config');
 const express = require('express');
-const https = require('https'); // Dodano
-const fs = require('fs'); // Dodano
+const https = require('https');
+const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,42 +12,43 @@ app.use(cors());
 
 const port = 3000;
 
+// Provjerava je li konfigurirana vanjska URL adresa
 if (config.externalUrl) {
+  // Ako je vanjska URL adresa konfigurirana, pokre?e HTTP server
   const hostname = '0.0.0.0';
   app.listen(config.port, hostname, () => {
     console.log(`Server locally running at http://${hostname}:${config.port}/ and from outside on ${config.externalUrl}`);
   });
 } else {
-  const https = require('https');  // Dodajte ovu liniju kako biste uklju?ili modul 'https'
-  const fs = require('fs');  
+  // Ako vanjska URL adresa nije konfigurirana, pokre?e HTTPS server
   const server = https.createServer({
     key: fs.readFileSync('server.key'),
     cert: fs.readFileSync('server.cert')
-  }, app)
-    .listen(config.port, function () {
-      console.log(`Server running at https://localhost:${config.port}/`);
-    });
+  }, app).listen(config.port, () => {
+    console.log(`Server running at https://localhost:${config.port}/`);
+  });
 }
 
-// Dodajte ovu rutu kako biste poslali HTML stranicu
+// Dodaje rutu za slanje HTML stranice
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'settings.html'));
 });
 
-// Rutiranje za pokretanje igre
-app.post('/start-game', (req, res) => {
-   // Dohvatite podatke iz tijela zahtjeva (req.body)
-   const { numAsteroids, asteroidFrequency } = req.body;
-
-   // Inicijalizirajte igru
-   const game = initializeGame(numAsteroids, asteroidFrequency);
- 
-   // Po?aljite odgovor
-   res.json({ success: true });
-});
-
 // Inicijalizacija igre
 function initializeGame(numAsteroids, asteroidFrequency) {
+  const canvas = document.getElementById("game");
+  const ctx = canvas.getContext("2d");
+  const PLAYER_WIDTH = 50;
+  const PLAYER_HEIGHT = 50;
+  const ASTEROID_WIDTH = 50;
+  const ASTEROID_HEIGHT = 50;
+  const ASTEROID_SPEED = 2;
+
+  let asteroids = [];
+  let bestTime = 0;
+  let startTime = 0;
+  let currentTime = 0;
+
   const player = {
     x: canvas.width / 2 - PLAYER_WIDTH / 2,
     y: canvas.height / 2 - PLAYER_HEIGHT / 2,
@@ -61,7 +61,7 @@ function initializeGame(numAsteroids, asteroidFrequency) {
   generateAsteroids();
   startTime = new Date();
 
-  // Postavite intervale za crtanje i generiranje asteroida
+  // Postavlja intervale za crtanje i generiranje asteroida
   setInterval(draw, 10);
   setInterval(function () {
     generateAsteroid();
@@ -76,165 +76,219 @@ function initializeGame(numAsteroids, asteroidFrequency) {
   };
 }
 
-// Ovaj dio je pomaknut iznad, izvan if-else bloka, tako da se izvr?ava uvijek kada je potrebno
+// Crtanje igre
+function draw() {
+  currentTime = new Date() - startTime;
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawPlayer();
+  for (let asteroid of asteroids) {
+    drawAsteroid(asteroid);
+  }
+  drawTime();
+
+  updatePlayer();
+  updateAsteroids();
+}
+
+// Vra?a funkciju za crtanje igra?a
+function drawPlayer() {
+  ctx.drawImage(playerImage, player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
+}
+
+// Vra?a funkciju za crtanje asteroida
+function drawAsteroid(asteroid) {
+  ctx.drawImage(asteroidImage, asteroid.x, asteroid.y, ASTEROID_WIDTH, ASTEROID_HEIGHT);
+}
+
+// Vra?a funkciju za a?uriranje polo?aja igra?a
+function updatePlayer() {
+  player.x += player.dx;
+  player.y += player.dy;
+
+  if (player.x > canvas.width) player.x = 0;
+  if (player.x < 0) player.x = canvas.width;
+  if (player.y > canvas.height) player.y = 0;
+  if (player.y < 0) player.y = canvas.height;
+}
+
+// Vra?a funkciju za a?uriranje polo?aja asteroida
+function updateAsteroids() {
+  for (let asteroid of asteroids) {
+    asteroid.x += asteroid.dx;
+    asteroid.y += asteroid.dy;
+
+    if (asteroid.x > canvas.width) asteroid.x = 0;
+    if (asteroid.x < 0) asteroid.x = canvas.width;
+    if (asteroid.y > canvas.height) asteroid.y = 0;
+    if (asteroid.y < 0) asteroid.y = canvas.height;
+
+    if (
+      player.x < asteroid.x + ASTEROID_WIDTH &&
+      player.x + PLAYER_WIDTH > asteroid.x &&
+      player.y < asteroid.y + ASTEROID_HEIGHT &&
+      player.y + PLAYER_HEIGHT > asteroid.y
+    ) {
+      handleCollision();
+    }
+  }
+}
+
+// Vra?a funkciju za crtanje vremena
+function drawTime() {
+  ctx.font = '16px Arial';
+  ctx.fillStyle = 'white';
+  ctx.textAlign = 'right';
+  ctx.fillText(`Best Time: ${formatTime(bestTime)}`, canvas.width - 10, 20);
+  ctx.fillText(`Current Time: ${formatTime(currentTime)}`, canvas.width - 10, 40);
+}
+
+// Formatira vrijeme u odgovaraju?i oblik
+function formatTime(time) {
+  const minutes = Math.floor(time / (60 * 1000));
+  const seconds = Math.floor((time % (60 * 1000)) / 1000);
+  const milliseconds = time % 1000;
+  return `${padZero(minutes)}:${padZero(seconds)}.${padZero(milliseconds, 3)}`;
+}
+
+// Dodaje nule ispred brojeva ako je potrebno
+function padZero(num, size = 2) {
+  let s = num + "";
+  while (s.length < size) s = "0" + s;
+  return s;
+}
+
+// Vra?a funkciju za rukovanje sudarom
+function handleCollision() {
+  currentTime = new Date() - startTime;
+
+  if (currentTime > bestTime) {
+    bestTime = currentTime;
+    localStorage.setItem("bestTime", bestTime);
+  }
+
+  resetGame();
+}
+
+// Vra?a funkciju za resetiranje igre
+function resetGame() {
+  player.x = canvas.width / 2 - PLAYER_WIDTH / 2;
+  player.y = canvas.height / 2 - PLAYER_HEIGHT / 2;
+  asteroids = [];
+  generateAsteroids();
+}
+
+// Vra?a funkciju za generiranje asteroida
+function generateAsteroids() {
+  for (let i = 0; i < numAsteroids; i++) {
+    generateAsteroid();
+  }
+}
+
+// Vra?a funkciju za generiranje pojedinog asteroida
+function generateAsteroid() {
+  asteroids.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    dx: (Math.random() - 0.5) * ASTEROID_SPEED,
+    dy: (Math.random() - 0.5) * ASTEROID_SPEED
+  });
+}
+
+// Dodaje funkciju za rukovanje tipkovni?kim doga?ajima
+function handleKeyDown(event) {
+  switch (event.key) {
+    case "ArrowLeft":
+      player.dx = -2;
+      break;
+    case "ArrowRight":
+      player.dx = 2;
+      break;
+    case "ArrowUp":
+      player.dy = -2;
+      break;
+    case "ArrowDown":
+      player.dy = 2;
+      break;
+  }
+}
+
+// Dodaje funkciju za rukovanje otpu?tanje tipke
+function handleKeyUp(event) {
+  switch (event.key) {
+    case "ArrowLeft":
+    case "ArrowRight":
+      player.dx = 0;
+      break;
+    case "ArrowUp":
+    case "ArrowDown":
+      player.dy = 0;
+      break;
+  }
+}
+
+// Provjerava postoji li dokument (koristi se za izbjegavanje gre?aka pri izvr?avanju na serveru)
 if (typeof document !== 'undefined') {
+  // Pokre?e se nakon ?to se dokument potpuno u?ita
+  window.onload = function () {
+    const canvas = document.getElementById("game");
 
-window.onload = function () {
-  const canvas = document.getElementById("game");
-
-  if (!canvas) {
-    console.error("Canvas element not found.");
-    return;
-  }
-
-  const ctx = canvas.getContext("2d");
-  const PLAYER_WIDTH = 50;
-  const PLAYER_HEIGHT = 50;
-  const ASTEROID_WIDTH = 50;
-  const ASTEROID_HEIGHT = 50;
-  const ASTEROID_SPEED = 2;
-
-  let numAsteroids;
-  let asteroidFrequency;
-  let player;
-  let asteroids;
-  let bestTime;
-  let startTime, currentTime;
-
-  const asteroidImage = new Image();
-  asteroidImage.src = "asteroid.png";
-  asteroidImage.onload = function () {
-    if (asteroidImage.width === 0 || asteroidImage.height === 0) {
-      console.error("Slika asteroida nije u?itana ispravno. Proverite putanju i veli?inu slike.");
+    if (!canvas) {
+      console.error("Canvas element not found.");
+      return;
     }
-  };
 
-  const playerImage = new Image();
-  playerImage.src = "spaceship.png";
-  playerImage.onload = function () {
-    if (playerImage.width === 0 || playerImage.height === 0) {
-      console.error("Slika igra?a nije u?itana ispravno. Proverite putanju i veli?inu slike.");
-    }
-  };
+    const ctx = canvas.getContext("2d");
+    const PLAYER_WIDTH = 50;
+    const PLAYER_HEIGHT = 50;
+    const ASTEROID_WIDTH = 50;
+    const ASTEROID_HEIGHT = 50;
+    const ASTEROID_SPEED = 2;
 
-  function drawPlayer() {
-    ctx.drawImage(playerImage, player.x, player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
-  }
+    let numAsteroids;
+    let asteroidFrequency;
+    let player;
+    let asteroids;
+    let bestTime;
+    let startTime, currentTime;
 
-  function drawAsteroid(asteroid) {
-    ctx.drawImage(asteroidImage, asteroid.x, asteroid.y, ASTEROID_WIDTH, ASTEROID_HEIGHT);
-  }
-
-  function updatePlayer() {
-    player.x += player.dx;
-    player.y += player.dy;
-
-    if (player.x > canvas.width) player.x = 0;
-    if (player.x < 0) player.x = canvas.width;
-    if (player.y > canvas.height) player.y = 0;
-    if (player.y < 0) player.y = canvas.height;
-  }
-
-  function updateAsteroids() {
-    for (let asteroid of asteroids) {
-      asteroid.x += asteroid.dx;
-      asteroid.y += asteroid.dy;
-
-      if (asteroid.x > canvas.width) asteroid.x = 0;
-      if (asteroid.x < 0) asteroid.x = canvas.width;
-      if (asteroid.y > canvas.height) asteroid.y = 0;
-      if (asteroid.y < 0) asteroid.y = canvas.height;
-
-      if (
-        player.x < asteroid.x + ASTEROID_WIDTH &&
-        player.x + PLAYER_WIDTH > asteroid.x &&
-        player.y < asteroid.y + ASTEROID_HEIGHT &&
-        player.y + PLAYER_HEIGHT > asteroid.y
-      ) {
-        handleCollision();
+    const asteroidImage = new Image();
+    asteroidImage.src = "asteroid.png";
+    asteroidImage.onload = function () {
+      if (asteroidImage.width === 0 || asteroidImage.height === 0) {
+        console.error("Slika asteroida nije u?itana ispravno. Provjerite putanju i veli?inu slike.");
       }
-    }
-  }
+    };
 
-  function handleCollision() {
-    currentTime = new Date() - startTime;
+    const playerImage = new Image();
+    playerImage.src = "spaceship.png";
+    playerImage.onload = function () {
+      if (playerImage.width === 0 || playerImage.height === 0) {
+        console.error("Slika igra?a nije u?itana ispravno. Provjerite putanju i veli?inu slike.");
+      }
+    };
 
-    if (currentTime > bestTime) {
-      bestTime = currentTime;
-      localStorage.setItem("bestTime", bestTime);
-    }
+    // Dodaje rukovanje tipkovni?kim doga?ajima
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
 
-    resetGame();
-  }
+    // Pokre?e igru
+    startGame();
+  };
+}
 
-  function resetGame() {
-    player.x = canvas.width / 2 - PLAYER_WIDTH / 2;
-    player.y = canvas.height / 2 - PLAYER_HEIGHT / 2;
-    asteroids = [];
-    generateAsteroids();
-  }
-
-  function generateAsteroids() {
-    for (let i = 0; i < numAsteroids; i++) {
-      generateAsteroid();
-    }
-  }
-
-  function generateAsteroid() {
-    asteroids.push({
-      x: Math.random() * canvas.width,
-      y: Math.random() * canvas.height,
-      dx: (Math.random() - 0.5) * ASTEROID_SPEED,
-      dy: (Math.random() - 0.5) * ASTEROID_SPEED
-    });
-  }
-
-  function drawTime() {
-    ctx.font = '16px Arial';
-    ctx.fillStyle = 'white';
-    ctx.textAlign = 'right';
-    ctx.fillText(`Best Time: ${formatTime(bestTime)}`, canvas.width - 10, 20);
-    ctx.fillText(`Current Time: ${formatTime(currentTime)}`, canvas.width - 10, 40);
-  }
-
-  function formatTime(time) {
-    const minutes = Math.floor(time / (60 * 1000));
-    const seconds = Math.floor((time % (60 * 1000)) / 1000);
-    const milliseconds = time % 1000;
-    return `${padZero(minutes)}:${padZero(seconds)}.${padZero(milliseconds, 3)}`;
-  }
-
-  function padZero(num, size = 2) {
-    let s = num + "";
-    while (s.length < size) s = "0" + s;
-    return s;
-  }
-
-  function draw() {
-    currentTime = new Date() - startTime;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    drawPlayer();
-    for (let asteroid of asteroids) {
-      drawAsteroid(asteroid);
-    }
-    drawTime();
-
-    updatePlayer();
-    updateAsteroids();
-  }
-
+// Dodaje funkciju za pokretanje igre
 function startGame() {
   event.preventDefault();
 
   const numAsteroids = document.getElementById("numAsteroids").value;
   const asteroidFrequency = document.getElementById("asteroidFrequency").value;
 
-  // Pohrani podatke u localStorage
+  // Pohranjuje podatke u localStorage
   localStorage.setItem("numAsteroids", numAsteroids);
   localStorage.setItem("asteroidFrequency", asteroidFrequency);
 
-  // Po?alji podatke na server
+  // ?alje podatke na server
   fetch("/start-game", {
     method: "POST",
     headers: {
@@ -244,59 +298,16 @@ function startGame() {
   })
     .then(response => {
       if (!response.ok) {
-        throw new Error(`HTTP greska! Status: ${response.status}`);
+        throw new Error(`HTTP gre?ka! Status: ${response.status}`);
       }
       return response.json();
     })
     .then(data => {
       console.log("Igra pokrenuta:", data);
-      // Redirect to game.html
+      // Preusmjerava na game.html
       window.location.href = "game.html";
     })
     .catch(error => {
       console.error("Gre?ka pri pokretanju igre:", error);
     });
 }
-
-
-  document.addEventListener("keydown", function (event) {
-    handleKeyDown(event);
-  });
-
-  document.addEventListener("keyup", function (event) {
-    handleKeyUp(event);
-  });
-
-  function handleKeyDown(event) {
-    switch (event.key) {
-      case "ArrowLeft":
-        player.dx = -2;
-        break;
-      case "ArrowRight":
-        player.dx = 2;
-        break;
-      case "ArrowUp":
-        player.dy = -2;
-        break;
-      case "ArrowDown":
-        player.dy = 2;
-        break;
-    }
-  }
-
-  function handleKeyUp(event) {
-    switch (event.key) {
-      case "ArrowLeft":
-      case "ArrowRight":
-        player.dx = 0;
-        break;
-      case "ArrowUp":
-      case "ArrowDown":
-        player.dy = 0;
-        break;
-    }
-  }
-
-}
-startGame();
-};
